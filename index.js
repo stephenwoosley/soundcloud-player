ad258f0203b19c9a547b02ed17514259d4c61ccb
Stephen Woosley <stephenrwoosley@gmail.com>
2019-06-23 00:42:10 -0400

    boilerplate server setup

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.set('view engine', 'handlebars');
+app.engine("hbs", exphbs({ defaultLayout: "main"}));
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+app.listen(PORT, function () {
+  console.log('listening on PORT ' + PORT);
+});
